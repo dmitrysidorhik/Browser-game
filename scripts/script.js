c9316f41cac8a4952fdaddb768cd1d9495b4ad35
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-03-10 17:45:14 +0300

    fix: let -> const

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 // ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
