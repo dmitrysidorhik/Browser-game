0f701142013ea74ff60dafb8b1779b5abe644aec
dmitrysidorhik <dmitrysidorhik@gmail.com>
2022-03-24 11:51:28 +0300

    work on the 5 lesson -> my mod

--- scripts/script.js
+++ scripts/script.js
@@ -6,28 +6,22 @@ let dy = -2;
 let coordX = canvas.width / 2;
 let coordY = canvas.height - 30;
 
-function draw() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+function drawBall() {
     ctx.beginPath();
     ctx.arc(coordX, coordY, 10, 0, Math.PI * 2);
     ctx.fill();
     if (coordX < canvas.width * 0.65) {
-        coordX += dx;
-        coordY += dy;
+        changeCoord_1();
         ctx.fillStyle = "#0095DD";
         ctx.fill();
     }
     if ((coordX >= canvas.width * 0.65) && (coordY >= canvas.height * 0.35)) {
-        dx = 0;
-        coordX += dx;
-        coordY += dy;
+        changeCoord_2();
         ctx.fillStyle = "#AA95AD";
         ctx.fill();
     }
     if ((coordY >= 30) && (coordY < canvas.height * 0.35)) {
-        dx = -2;
-        coordX += dx;
-        coordY += dy;
+        changeCoord_3();
         ctx.fillStyle = "#BA15AD";
         ctx.fill();
         console.log(coordX);
@@ -35,8 +29,32 @@ function draw() {
     }
     if (coordY === 28 && coordX === 230) {
         ctx.closePath();
-		clearInterval(timerId);
+        clearInterval(timerId);
         return;
     }
 }
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+}
+
+function changeCoord_1() {
+    coordX += dx;
+    coordY += dy;
+}
+
+function changeCoord_2() {
+    dx = 0;
+    coordX += dx;
+    coordY += dy;
+}
+
+function changeCoord_3() {
+    dx = -2;
+    coordX += dx;
+    coordY += dy;
+}
+
 let timerId = setInterval(draw, 10);
+
